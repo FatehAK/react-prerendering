7b130e1832a56ee42990ff4e04d880a6846b3acb
Fateh Ali Khan <fatehalikhanh@gmail.com>
2019-09-08 22:40:36 +0530

    Add redux

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../../actions/index';
 import './Home.css';
 import img from '../../img/home.jpg';
 
@@ -7,6 +9,15 @@ class Home extends React.Component {
     state = {
         text: 'Home (from state)'
     };
+
+    async componentDidMount() {
+        // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        // const data = await response.json();
+        // this.setState({
+        //     posts: data.slice(0, 5)
+        // });
+        this.props.fetchPosts();
+    }
 
     render() {
         return (
@@ -16,10 +27,17 @@ class Home extends React.Component {
                 </Helmet>
                 <div className="home-state">{this.state.text}</div>
                 <div className="home-props">{this.props.text}</div>
+                {this.props.posts && this.props.posts.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
                 <img src={img} alt="home" width="200px" height="150px" />
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => ({
+    posts: state.posts
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Home);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
-    ReactDOM.hydrate(<BrowserRouter><App /></BrowserRouter>, rootElement);
+    ReactDOM.hydrate(
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>,
+        rootElement);
 } else {
-    ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, rootElement);
+    ReactDOM.render(
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>,
+        rootElement);
 }
 
 serviceWorker.unregister();
