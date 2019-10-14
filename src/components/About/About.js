1b8901c98d20a67e7d306ca1d7fb337de868afb4
fatehak <fa7ehak@gmail.com>
2019-10-14 18:18:33 +0530

    add link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { NavLink } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import './About.css';
 import img from '../../img/about.jpg';
 
@@ -8,12 +10,20 @@ class About extends React.Component {
         text: 'About us (from state)'
     };
 
+    handleClick() {
+        ReactGA.event({
+            category: 'LINK_CLICK',
+            action: 'User clicked link',
+            label: 'ABOUT_PAGE'
+        });
+    }
     render() {
         return (
             <div className="about">
                 <Helmet>
                     <title>About Page</title>
                 </Helmet>
+                <NavLink to="/contact" onClick={() => this.handleClick()}>Goto About</NavLink>
                 <div className="about-state">{this.state.text}</div>
                 <div className="about-props">{this.props.text}</div>
                 <img src={img} alt="about" width="200px" height="150px" />
